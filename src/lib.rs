a6af31033549a78f51389fc373a20ca48763cc34
Stephen Sherratt <sfsherratt@gmail.com>
2019-02-02 22:11:19 +0000

    Reorder for clarity, minor ergonomic changes, extra test

--- src/lib.rs
+++ src/lib.rs
@@ -6,32 +6,6 @@ extern crate coord_2d;
 use coord_2d::Axis;
 pub use coord_2d::Coord;
 
-#[derive(Debug, Clone, Copy)]
-pub struct GeneralLineSegmentIter<S: StepsTrait> {
-    steps: S,
-    current_coord: Coord,
-    remaining: usize,
-}
-
-pub type LineSegmentIter = GeneralLineSegmentIter<Steps>;
-pub type LineSegmentIterCardinal = GeneralLineSegmentIter<StepsCardinal>;
-
-pub trait TraverseTrait: Clone + Eq + private::Sealed {
-    type Steps: StepsTrait;
-    type Iter: Iterator<Item = Coord>;
-    fn num_steps(&self) -> usize;
-    fn steps(&self) -> Self::Steps;
-    fn iter(&self) -> Self::Iter;
-    fn iter_config(&self, config: Config) -> Self::Iter;
-    fn line_segment(&self) -> LineSegment;
-    fn start(&self) -> Coord {
-        self.line_segment().start
-    }
-    fn end(&self) -> Coord {
-        self.line_segment().end
-    }
-}
-
 pub trait StepsTrait: Clone + Eq + private::Sealed {
     fn prev(&mut self) -> Coord;
     fn next(&mut self) -> Coord;
@@ -42,159 +16,6 @@ pub trait StepsTrait: Clone + Eq + private::Sealed {
     fn next_copy(&self) -> (Coord, Self) {
         let mut s = self.clone();
         (s.next(), s)
-    }
-}
-
-#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
-#[derive(Default, Debug, Clone, Copy)]
-pub struct Config {
-    pub exclude_start: bool,
-    pub exclude_end: bool,
-}
-
-impl Config {
-    pub fn new() -> Self {
-        Default::default()
-    }
-    pub fn include_start(self) -> Self {
-        Self {
-            exclude_start: false,
-            ..self
-        }
-    }
-    pub fn include_end(self) -> Self {
-        Self {
-            exclude_end: false,
-            ..self
-        }
-    }
-    pub fn exclude_start(self) -> Self {
-        Self {
-            exclude_start: true,
-            ..self
-        }
-    }
-    pub fn exclude_end(self) -> Self {
-        Self {
-            exclude_end: true,
-            ..self
-        }
-    }
-}
-
-#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct LineSegment {
-    pub start: Coord,
-    pub end: Coord,
-}
-
-#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Traverse {
-    pub line_segment: LineSegment,
-}
-
-#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct TraverseCardinal {
-    pub line_segment: LineSegment,
-}
-
-impl LineSegment {
-    pub fn new(start: Coord, end: Coord) -> Self {
-        Self { start, end }
-    }
-    pub fn traverse(&self) -> Traverse {
-        Traverse {
-            line_segment: *self,
-        }
-    }
-    pub fn traverse_cardinal(&self) -> TraverseCardinal {
-        TraverseCardinal {
-            line_segment: *self,
-        }
-    }
-    pub fn delta(&self) -> Coord {
-        self.end - self.start
-    }
-    pub fn iter(&self) -> LineSegmentIter {
-        GeneralLineSegmentIter::new(self.traverse())
-    }
-    pub fn iter_cardinal(&self) -> LineSegmentIterCardinal {
-        GeneralLineSegmentIter::new(self.traverse_cardinal())
-    }
-    pub fn iter_config(&self, config: Config) -> LineSegmentIter {
-        GeneralLineSegmentIter::new_config(self.traverse(), config)
-    }
-    pub fn iter_cardinal_config(&self, config: Config) -> LineSegmentIterCardinal {
-        GeneralLineSegmentIter::new_config(self.traverse_cardinal(), config)
-    }
-    pub fn steps(&self) -> Steps {
-        Steps::new(self.delta())
-    }
-    pub fn steps_cardinal(&self) -> StepsCardinal {
-        StepsCardinal::new(self.delta())
-    }
-    pub fn num_steps(&self) -> usize {
-        let delta = self.delta();
-        delta.x.abs().max(delta.y.abs()) as usize + 1
-    }
-    pub fn num_steps_cardinal(&self) -> usize {
-        let delta = self.delta();
-        delta.x.abs() as usize + delta.y.abs() as usize + 1
-    }
-    pub fn reverse(&self) -> Self {
-        Self {
-            start: self.end,
-            end: self.start,
-        }
-    }
-    pub fn try_infinite(&self) -> Result<InfiniteLineSegment, ZeroLengthDelta> {
-        InfiniteLineSegment::try_new(self.start, self.delta())
-    }
-    pub fn infinite(&self) -> InfiniteLineSegment {
-        self.try_infinite().unwrap()
-    }
-}
-
-impl TraverseTrait for Traverse {
-    type Steps = Steps;
-    type Iter = LineSegmentIter;
-    fn num_steps(&self) -> usize {
-        self.line_segment.num_steps()
-    }
-    fn steps(&self) -> Self::Steps {
-        self.line_segment.steps()
-    }
-    fn iter(&self) -> Self::Iter {
-        self.line_segment.iter()
-    }
-    fn iter_config(&self, config: Config) -> Self::Iter {
-        self.line_segment.iter_config(config)
-    }
-    fn line_segment(&self) -> LineSegment {
-        self.line_segment
-    }
-}
-
-impl TraverseTrait for TraverseCardinal {
-    type Steps = StepsCardinal;
-    type Iter = LineSegmentIterCardinal;
-    fn num_steps(&self) -> usize {
-        self.line_segment.num_steps_cardinal()
-    }
-    fn steps(&self) -> Self::Steps {
-        self.line_segment.steps_cardinal()
-    }
-    fn iter(&self) -> LineSegmentIterCardinal {
-        self.line_segment.iter_cardinal()
-    }
-    fn iter_config(&self, config: Config) -> LineSegmentIterCardinal {
-        self.line_segment.iter_cardinal_config(config)
-    }
-    fn line_segment(&self) -> LineSegment {
-        self.line_segment
     }
 }
 
@@ -314,6 +135,185 @@ impl StepsTrait for StepsCardinal {
     }
 }
 
+#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct LineSegment {
+    pub start: Coord,
+    pub end: Coord,
+}
+
+impl LineSegment {
+    pub fn new(start: Coord, end: Coord) -> Self {
+        Self { start, end }
+    }
+    pub fn traverse(&self) -> Traverse {
+        Traverse {
+            line_segment: *self,
+        }
+    }
+    pub fn traverse_cardinal(&self) -> TraverseCardinal {
+        TraverseCardinal {
+            line_segment: *self,
+        }
+    }
+    pub fn delta(&self) -> Coord {
+        self.end - self.start
+    }
+    pub fn iter(&self) -> LineSegmentIter {
+        GeneralLineSegmentIter::new(self.traverse())
+    }
+    pub fn iter_cardinal(&self) -> LineSegmentIterCardinal {
+        GeneralLineSegmentIter::new(self.traverse_cardinal())
+    }
+    pub fn iter_config(&self, config: Config) -> LineSegmentIter {
+        GeneralLineSegmentIter::new_config(self.traverse(), config)
+    }
+    pub fn iter_cardinal_config(&self, config: Config) -> LineSegmentIterCardinal {
+        GeneralLineSegmentIter::new_config(self.traverse_cardinal(), config)
+    }
+    pub fn steps(&self) -> Steps {
+        Steps::new(self.delta())
+    }
+    pub fn steps_cardinal(&self) -> StepsCardinal {
+        StepsCardinal::new(self.delta())
+    }
+    pub fn num_steps(&self) -> usize {
+        let delta = self.delta();
+        delta.x.abs().max(delta.y.abs()) as usize + 1
+    }
+    pub fn num_steps_cardinal(&self) -> usize {
+        let delta = self.delta();
+        delta.x.abs() as usize + delta.y.abs() as usize + 1
+    }
+    pub fn reverse(&self) -> Self {
+        Self {
+            start: self.end,
+            end: self.start,
+        }
+    }
+    pub fn try_infinite(&self) -> Result<InfiniteLineSegment, ZeroLengthDelta> {
+        InfiniteLineSegment::try_new(self.start, self.delta())
+    }
+    pub fn infinite(&self) -> InfiniteLineSegment {
+        self.try_infinite().unwrap()
+    }
+}
+
+pub trait TraverseTrait: Clone + Eq + private::Sealed {
+    type Steps: StepsTrait;
+    type Iter: Iterator<Item = Coord>;
+    fn num_steps(&self) -> usize;
+    fn steps(&self) -> Self::Steps;
+    fn iter(&self) -> Self::Iter;
+    fn iter_config(&self, config: Config) -> Self::Iter;
+    fn line_segment(&self) -> LineSegment;
+    fn start(&self) -> Coord {
+        self.line_segment().start
+    }
+    fn end(&self) -> Coord {
+        self.line_segment().end
+    }
+}
+
+#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Traverse {
+    pub line_segment: LineSegment,
+}
+
+#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct TraverseCardinal {
+    pub line_segment: LineSegment,
+}
+
+impl TraverseTrait for Traverse {
+    type Steps = Steps;
+    type Iter = LineSegmentIter;
+    fn num_steps(&self) -> usize {
+        self.line_segment.num_steps()
+    }
+    fn steps(&self) -> Self::Steps {
+        self.line_segment.steps()
+    }
+    fn iter(&self) -> Self::Iter {
+        self.line_segment.iter()
+    }
+    fn iter_config(&self, config: Config) -> Self::Iter {
+        self.line_segment.iter_config(config)
+    }
+    fn line_segment(&self) -> LineSegment {
+        self.line_segment
+    }
+}
+
+impl TraverseTrait for TraverseCardinal {
+    type Steps = StepsCardinal;
+    type Iter = LineSegmentIterCardinal;
+    fn num_steps(&self) -> usize {
+        self.line_segment.num_steps_cardinal()
+    }
+    fn steps(&self) -> Self::Steps {
+        self.line_segment.steps_cardinal()
+    }
+    fn iter(&self) -> LineSegmentIterCardinal {
+        self.line_segment.iter_cardinal()
+    }
+    fn iter_config(&self, config: Config) -> LineSegmentIterCardinal {
+        self.line_segment.iter_cardinal_config(config)
+    }
+    fn line_segment(&self) -> LineSegment {
+        self.line_segment
+    }
+}
+
+#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
+#[derive(Default, Debug, Clone, Copy)]
+pub struct Config {
+    pub exclude_start: bool,
+    pub exclude_end: bool,
+}
+
+impl Config {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn include_start(self) -> Self {
+        Self {
+            exclude_start: false,
+            ..self
+        }
+    }
+    pub fn include_end(self) -> Self {
+        Self {
+            exclude_end: false,
+            ..self
+        }
+    }
+    pub fn exclude_start(self) -> Self {
+        Self {
+            exclude_start: true,
+            ..self
+        }
+    }
+    pub fn exclude_end(self) -> Self {
+        Self {
+            exclude_end: true,
+            ..self
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct GeneralLineSegmentIter<S: StepsTrait> {
+    steps: S,
+    current_coord: Coord,
+    remaining: usize,
+}
+
+pub type LineSegmentIter = GeneralLineSegmentIter<Steps>;
+pub type LineSegmentIterCardinal = GeneralLineSegmentIter<StepsCardinal>;
+
 impl<S: StepsTrait> GeneralLineSegmentIter<S> {
     fn new<T: TraverseTrait<Steps = S>>(traverse: T) -> Self {
         let mut steps = traverse.steps();
@@ -377,20 +377,6 @@ impl IntoIterator for TraverseCardinal {
 }
 
 #[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
-pub struct InfiniteConfig {
-    exclude_start: bool,
-}
-
-pub trait InfiniteTraverseTrait: Clone + Eq + private::Sealed {
-    type Steps: StepsTrait;
-    type Iter: Iterator<Item = Coord>;
-    fn steps(&self) -> Self::Steps;
-    fn iter(&self) -> Self::Iter;
-    fn iter_config(&self, config: InfiniteConfig) -> Self::Iter;
-    fn start(&self) -> Coord;
-}
-
-#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct InfiniteLineSegment {
     line_segment: LineSegment,
@@ -416,7 +402,7 @@ impl InfiniteLineSegment {
     pub fn start(&self) -> Coord {
         self.line_segment.start
     }
-    fn delta(&self) -> Coord {
+    pub fn delta(&self) -> Coord {
         self.line_segment.delta()
     }
     pub fn reverse(&self) -> Self {
@@ -458,49 +444,13 @@ impl InfiniteLineSegment {
     }
 }
 
-#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
-#[derive(Debug, Clone, Copy)]
-pub struct GeneralInfiniteLineSegmentIter<S: StepsTrait> {
-    steps: S,
-    current_coord: Coord,
-}
-
-pub type InfiniteLineSegmentIter = GeneralInfiniteLineSegmentIter<Steps>;
-pub type InfiniteLineSegmentIterCardinal = GeneralInfiniteLineSegmentIter<StepsCardinal>;
-
-impl<S: StepsTrait> Iterator for GeneralInfiniteLineSegmentIter<S> {
-    type Item = Coord;
-    fn next(&mut self) -> Option<Self::Item> {
-        let (delta, next_steps) = self.steps.next_copy();
-        if let Some(next_coord) = self.current_coord.checked_add(delta) {
-            self.current_coord = next_coord;
-            self.steps = next_steps;
-            Some(next_coord)
-        } else {
-            None
-        }
-    }
-}
-
-impl<S: StepsTrait> GeneralInfiniteLineSegmentIter<S> {
-    fn new<T: InfiniteTraverseTrait<Steps = S>>(traverse: T) -> Self {
-        let mut steps = traverse.steps();
-        let backwards_step = steps.prev();
-        let current_coord = traverse.start() + backwards_step;
-        Self {
-            steps,
-            current_coord,
-        }
-    }
-
-    fn new_exclude_start<T: InfiniteTraverseTrait<Steps = S>>(traverse: T) -> Self {
-        let steps = traverse.steps();
-        let current_coord = traverse.start();
-        Self {
-            steps,
-            current_coord,
-        }
-    }
+pub trait InfiniteTraverseTrait: Clone + Eq + private::Sealed {
+    type Steps: StepsTrait;
+    type Iter: Iterator<Item = Coord>;
+    fn steps(&self) -> Self::Steps;
+    fn iter(&self) -> Self::Iter;
+    fn iter_config(&self, config: InfiniteConfig) -> Self::Iter;
+    fn start(&self) -> Coord;
 }
 
 #[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
@@ -546,6 +496,75 @@ impl InfiniteTraverseTrait for InfiniteTraverseCardinal {
     }
     fn start(&self) -> Coord {
         self.line_segment.start()
+    }
+}
+
+#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
+#[derive(Default, Debug, Clone, Copy)]
+pub struct InfiniteConfig {
+    pub exclude_start: bool,
+}
+
+impl InfiniteConfig {
+    pub fn new() -> Self {
+        Default::default()
+    }
+    pub fn include_start(self) -> Self {
+        Self {
+            exclude_start: false,
+            ..self
+        }
+    }
+    pub fn exclude_start(self) -> Self {
+        Self {
+            exclude_start: true,
+            ..self
+        }
+    }
+}
+
+#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, Copy)]
+pub struct GeneralInfiniteLineSegmentIter<S: StepsTrait> {
+    steps: S,
+    current_coord: Coord,
+}
+
+pub type InfiniteLineSegmentIter = GeneralInfiniteLineSegmentIter<Steps>;
+pub type InfiniteLineSegmentIterCardinal = GeneralInfiniteLineSegmentIter<StepsCardinal>;
+
+impl<S: StepsTrait> GeneralInfiniteLineSegmentIter<S> {
+    fn new<T: InfiniteTraverseTrait<Steps = S>>(traverse: T) -> Self {
+        let mut steps = traverse.steps();
+        let backwards_step = steps.prev();
+        let current_coord = traverse.start() + backwards_step;
+        Self {
+            steps,
+            current_coord,
+        }
+    }
+
+    fn new_exclude_start<T: InfiniteTraverseTrait<Steps = S>>(traverse: T) -> Self {
+        let steps = traverse.steps();
+        let current_coord = traverse.start();
+        Self {
+            steps,
+            current_coord,
+        }
+    }
+}
+
+impl<S: StepsTrait> Iterator for GeneralInfiniteLineSegmentIter<S> {
+    type Item = Coord;
+    fn next(&mut self) -> Option<Self::Item> {
+        let (delta, next_steps) = self.steps.next_copy();
+        if let Some(next_coord) = self.current_coord.checked_add(delta) {
+            self.current_coord = next_coord;
+            self.steps = next_steps;
+            Some(next_coord)
+        } else {
+            None
+        }
     }
 }
 
@@ -647,6 +666,11 @@ mod test {
         f.iter().zip(i.iter()).for_each(|(f, i)| {
             assert_eq!(f, i);
         });
+        f.iter_config(Config::new().exclude_start())
+            .zip(i.iter_config(InfiniteConfig::new().exclude_start()))
+            .for_each(|(f, i)| {
+                assert_eq!(f, i);
+            });
     }
 
     fn test_properties(line_segment: LineSegment) {
